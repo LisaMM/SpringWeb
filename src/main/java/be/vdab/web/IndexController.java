f7d7768384d6960cae77ca88d8f83c486c764173
Lisa <lisa.m.m.steendam@gmail.com>
2014-06-05 11:45:09 +0200

    use case: kleur veranderen indexpagina

--- src/main/java/be/vdab/web/IndexController.java
+++ src/main/java/be/vdab/web/IndexController.java
@@ -11,17 +11,28 @@ import be.vdab.services.FiliaalService;
 @RequestMapping("/")
 class IndexController {
 	private final FiliaalService filiaalService;
+	private final Voorkeuren voorkeuren;
 	
 	@Autowired
-	public IndexController(FiliaalService filiaalService) {
+	public IndexController(FiliaalService filiaalService, Voorkeuren voorkeuren) {
 		this.filiaalService = filiaalService;
+		this.voorkeuren = voorkeuren;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("aantalFilialen", 
-			filiaalService.findAantalFilialen());
+		modelAndView.addObject("aantalFilialen", filiaalService.findAantalFilialen());
+		modelAndView.addObject("kleur", voorkeuren.getAchtergrondkleur());
+		return modelAndView;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, params = "kleur")
+	public ModelAndView kleurKeuze(@RequestParam String kleur) {
+		voorkeuren.setAchtergrondkleur(kleur);
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("aantalFilialen", filiaalService.findAantalFilialen());
+		modelAndView.addObject("kleur", kleur);
 		return modelAndView;
 	}
 }
